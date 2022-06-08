113a8a6e2efcf44e3a804be1067d5192250dfb2c
arron <arron.moss@zero1.co.uk>
2022-06-08 22:54:56 +0100

    working post scores

--- mmm/record.js
+++ mmm/record.js
@@ -16,18 +16,21 @@ function drawFixtureReport() {
     var liveUrl = "https://script.google.com/macros/s/AKfycbx4uriTLlVllJfZG0TMXTww1T90JqQJyV1D2C7QbvoMWT29KJk/exec?table=ScoreReport&agegroup="+agegroup;
     var table = new google.visualization.Table(document.getElementById('table_fixtures'));
     var dataTable = new google.visualization.DataTable();
+
+    dataTable.addColumn('number', 'ID');
     dataTable.addColumn('string', 'Time');
-    dataTable.addColumn('string', 'Group');
-    dataTable.addColumn('string', 'Pitch');
+    //dataTable.addColumn('string', 'Group');
+    //dataTable.addColumn('string', 'Pitch');
     dataTable.addColumn('string', 'Home');
     dataTable.addColumn('number', '');
     dataTable.addColumn('string', 'Away');
     dataTable.addColumn('number', '');
+    dataTable.addColumn('string', 'Save');
 
-    var leagueTable = [];
+    var leagueTable = []; var id = 11;
+
 
     $.getJSON( liveUrl , function( data ) {
-
         // Check the time and only report ones with no data which are in the past
         $.each( data, function( key, team ) {
             //console.log(team[0]);
@@ -39,18 +42,47 @@ function drawFixtureReport() {
                 if(team[4]==='' && team[6]==='') {
                     // Only push the score sheet to UX if scores empty
                     team[4]=null;team[6]=null;
-                    leagueTable.push( team );
-
-
+                    leagueTable.push( [id,team[0],team[3],team[4],team[5],team[6],'<button class="button" style="width:100%;">SAVE</button>'] );
                 }
-
             } else {
                 console.log('dont show result');
             }
+            id++; // increment the ID so we know which row to post back to
 
         });
         dataTable.addRows(leagueTable);
-        table.draw(dataTable, {showRowNumber: false, width: '100%', height: '50%'});
+        table.draw(dataTable, {allowHtml:true, showRowNumber: false, width: '100%', height: '50%'});
+
+        $(".google-visualization-table-type-number.google-visualization-table-td").attr("contenteditable","true");
+
+        $(".button").click(function(){
+            console.log('diddly');
+            console.log($(this).parent().parent().html());
+            html = $.parseHTML( $(this).parent().parent().html() )
+            //
+            var homescore = $(html[3]).text();
+            var awayscore = $(html[5]).text();
+            console.log(typeof homescore);
+            console.log(typeof awayscore);
+            $.post( "https://script.google.com/macros/s/AKfycbx4uriTLlVllJfZG0TMXTww1T90JqQJyV1D2C7QbvoMWT29KJk/exec",
+                { id: $(html[0]).text(),
+                    home: homescore,
+                    away: awayscore,
+                    agegroup: "Mini"})
+                .done(function( data ) {
+                    alert( "Data Loaded: " + data );
+                });
+
+
+
+
+        });
+        $(document).on("click", "a.remove" , function() {
+            $(this).parent().remove();
+        });
+
+
+
     });
 }
 
@@ -59,12 +91,7 @@ function drawFixtureReport() {
 
 
 
-
-
-
-
 jQuery(document).ready(function(){
-
     // Wait for google tables to appear in DOM
     var waitForEl = function(selector, callback) {
         if (jQuery(selector).length) {
@@ -78,7 +105,7 @@ jQuery(document).ready(function(){
 
     waitForEl('.google-visualization-table-table', function() {
         addClassesToTablesForFlags();
-        checkIfFinals();
+        //checkIfFinals();
     });
 
     function setNewQueryStringOnRefreshButton() {
@@ -86,20 +113,6 @@ jQuery(document).ready(function(){
         var refreshButton = jQuery('#refresh');
         var newHref = '/mmm/' + agegroup + '.html?' + randomStringForQueryString;
         jQuery(refreshButton).attr('href', newHref);
-    }
-
-    function checkIfFinals() {
-        if(allGamesPlayed===false)  {
-            jQuery('#finals').hide();
-            console.log('all games NOT played');
-        } else {
-            jQuery('#finals').show();
-        }
-    }
-
-    function enableAdmins() {
-        // checks required, URL query sets cookie?
-        $(".google-visualization-table-type-number.google-visualization-table-td").attr("contenteditable","true");
     }
 
     function addClassesToTablesForFlags() {
@@ -126,8 +139,6 @@ jQuery(document).ready(function(){
             }
         });
     }
-
     setNewQueryStringOnRefreshButton();
-    enableAdmins();
 
 });
