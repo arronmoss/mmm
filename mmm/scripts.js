50582f6493349dd804698e8869413ac922c09938
Arron Moss <arron.moss@zero1.co.uk>
2023-07-02 12:14:58 +0100

    Update scripts.js

--- mmm/scripts.js
+++ mmm/scripts.js
@@ -12,15 +12,21 @@ var allGamesPlayed = true;
 var dataUrl = "https://script.google.com/macros/s/AKfycbxg1MzLm6uVpMENFW5PcwKCpRtNcUXJkkETM741taPnUybHsgXJ_d6ejPwZmD3uKBPK5w/exec";
 
 var redirectagegroup = agegroup.split('-').shift();
-if(document.location.search.includes('?refresh='))
+if(document.location.search.includes('?refresh=')) {
     var timer = document.location.search.replace("?refresh=", "");
-if(timer<1000) timer = 1000;
-if(redirectagegroup==='Mini') {
-    var switchgroup = 'Senior';
-} else {
-    var switchgroup = 'Mini';
+
+    if(timer<1000) timer = 1000;
+    if(redirectagegroup==='Mini') {
+        var switchgroup = 'Senior';
+    } else {
+        var switchgroup = 'Mini';
+    }
+    setTimeout(function(){location.href=`https://buxtonjfc.uk/mmm/${switchgroup}.html?refresh=${timer}`} , 10000); 
+
+
+
 }
-setTimeout(function(){location.href=`https://buxtonjfc.uk/mmm/${switchgroup}.html?refresh=${timer}`} , 10000); 
+
 
 function drawTableFinals() {
     var liveUrl = `${dataUrl}?table=${agegroup}-Finals`;
